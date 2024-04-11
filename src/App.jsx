7fa533ef90f591ecf375f9830f26eed305022b0b
MFletcherstudying <107318048+MFletcherstudying@users.noreply.github.com>
2024-04-11 05:38:18 +0000

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Marc Fletcher",
+  title: "Entreprenuer",
+  email: "marc@velflet.com",
+  gitHub: "https://github.com/MFletcherstudying",
+  instagram: "@m_anthony_f",
+  linkedIn: "https://www.linkedin.com/in/marc-a-fletcher/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
